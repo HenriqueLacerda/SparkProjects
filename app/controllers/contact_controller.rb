0a794213e35186b78fba569d48b3ec8d441103dc
henrique <geraldohenriquelacerda@gmail.com>
2013-09-05 18:34:45 -0300

    Adicionando o controller contact

---
+++ app/controllers/contact_controller.rb
@@ -0,0 +1,12 @@
+class ContactController < ApplicationController
+
+  def index
+    @projects = Project.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @projects }
+    end
+  end
+  
+end
