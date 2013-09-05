3ab1d7f48dfab15c91280ec9faf99fd707230c88
henrique <geraldohenriquelacerda@gmail.com>
2013-09-05 17:45:47 -0300

    Adicionando rotas para os scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 SparkProjects::Application.routes.draw do
+  resources :team_members
+
+
+  resources :teams
+
+
+  resources :task_types
+
+
+  resources :task_stats
+
+
+  resources :tasks
+
+
   resources :projects
-
-
+  resources :dashboard
   devise_for :users
 
+  root :to => 'dashboard#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
