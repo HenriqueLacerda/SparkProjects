bdef3693dd7f886fd70e5aec11fd2b1cb370fef7
henrique <geraldohenriquelacerda@gmail.com>
2013-09-05 17:45:23 -0300

    Adicionando scaffold de Dashboard

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,12 @@
+class DashboardController < ApplicationController
+  
+  def index
+    @projects = Project.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @projects }
+    end
+  end
+  
+end
