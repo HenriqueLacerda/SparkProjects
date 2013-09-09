dcacde5cacf7c8e136b7e8207ec15bb23b74b44d
henrique <geraldohenriquelacerda@gmail.com>
2013-09-09 18:54:45 -0300

    Tentativas de configuração da Brazilian Rails

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SparkProjects::Application.initialize!
+#brazilian-rails
+#require 'brstring'
+#require 'brdata'
+#require 'brdinheiro'
+#require 'brI18n'
