9f55b0b495b8117b218bb4c224687830ed4157a4
henrique <geraldohenriquelacerda@gmail.com>
2013-09-09 12:27:13 -0300

    Adicionando Validacaoes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
+  validates_presence_of :nome
+  validates_presence_of :dataInicio
   attr_accessible :dataFinal, :dataInicio, :descricao, :nome
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,6 @@
 class Team < ActiveRecord::Base
+  validates_presence_of :nome, message: "Digite o nome da Equipe"
+  validates_presence_of :user, message: "Selecione o usuario responsavel pela Equipe"
   belongs_to :user
-  attr_accessible :nome
+  attr_accessible :nome, :user_id
 end
